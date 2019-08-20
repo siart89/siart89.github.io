c26e4f2b3d1ed6e229318fac78b8c19a94f2f964
siart89 <artyomreva@gmail.com>
2019-08-20 21:33:11 +0300

    доделано всплывающее окно

--- tryToDo/js/main.js
+++ tryToDo/js/main.js
@@ -1,22 +1,27 @@
 'use strict';
 
+// открыте окна запись на Консультацию
 function showFreeConsult() {
     const openBtn = document.querySelector('.header_contacts__btn'),
         closeBtn = document.querySelector('.close'),
+        form = document.querySelector('.form_wrapper'),
+        closeBtnBot = document.querySelector('.modal_bot__close'),
         modal = document.querySelector('.freeConsult_window');
 
     openBtn.addEventListener('click', (e) => {
         modal.style.display = 'block';
+        document.querySelector('.usr_name').focus();
         document.body.style.overflow = 'hidden';
+        form.classList.add('transform');
     });
-    closeBtn.addEventListener('click', () => {
-        modal.style.display = '';
-        document.body.style.overflow = '';
-    })
-    modal.addEventListener('click', function(e){
-        if (e.target !== this) return;
+
+    modal.addEventListener('click', function (e) {
+        if (!(e.target == this || e.target == closeBtn || e.target == closeBtnBot)) return;
+
         this.style.display = '';
         document.body.style.overflow = '';
+        form.classList.remove('transform');
     })
-}
+};
+
 showFreeConsult();
